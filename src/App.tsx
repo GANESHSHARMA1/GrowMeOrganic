dc18295d62dbb0066ab461747917448957258e83
GANESHSHARMA1 <ganeshkithana@gmail.com>
2024-09-16 23:12:05 +0530

    Assignment Submission

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import Pagination from './Components/Pagination'
+
+function App() {
+
+  return (
+    <>
+      <Pagination />
+    </>
+  )
+}
+
+export default App
